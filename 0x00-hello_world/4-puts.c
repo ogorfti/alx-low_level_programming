c204f8f91528a574539b931ff945a1dbeb9fdf11
ogorfti <ww.shanks1@gmail.com>
2023-06-15 13:47:06 -0700

    fix betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - A program that prints a string using puts.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
