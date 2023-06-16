7d5bb419b745d67efd8809199594329c692d0baa
ogorfti <ww.shanks1@gmail.com>
2023-06-16 02:45:11 -0700

    Add C program to print the alphabet in lowercase (except q and e) using putchar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ * (except q and e) using putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
