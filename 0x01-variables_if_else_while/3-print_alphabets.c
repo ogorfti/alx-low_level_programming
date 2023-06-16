8b157b8e84f851e7dfc0ca2fb8fb9e4f452ced23
ogorfti <ww.shanks1@gmail.com>
2023-06-16 02:39:18 -0700

    Add C program to print the alphabet in lowercase and uppercase using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ *  and uppercase using putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
