3a10828c650446f2482e09919b253cc074b0465c
ogorfti <ww.shanks1@gmail.com>
2023-06-20 03:46:37 -0700

    Add function to compute the absolute value of an integer using _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to compute the absolute value of.
+ *
+ * Return: The absolute value of n.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
+
