ac73bb2988bd58d2b39ed8e4fef84d81bacfeea7
ogorfti <ww.shanks1@gmail.com>
2023-06-20 04:06:30 -0700

    Add function to print the last digit of a number using print_last_digit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98.
+ * @n: The starting number.
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n < 98; n++)
+			printf("%d, ", n);
+	}
+	else
+	{
+		for (; n > 98; n--)
+			printf("%d, ", n);
+	}
+	printf("98\n");
+}
