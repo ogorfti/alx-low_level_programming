23084cc1621cb60c418a638a8697e18f249f7a29
ogorfti <ww.shanks1@gmail.com>
2023-06-20 03:16:43 -0700

    Add function to print alphabet in lowercase using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
