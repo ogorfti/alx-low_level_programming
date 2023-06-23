558b6186cf51a9a1b4f28d0c2bd3771c92772652
ogorfti <ww.shanks1@gmail.com>
2023-06-23 06:45:01 -0700

    Add function to print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+/**
+ * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+ * Return: void
+ */
+void  print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
