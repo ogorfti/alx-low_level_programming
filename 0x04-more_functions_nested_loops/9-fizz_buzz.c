6a12b95148fef07848737bbdd08473620068364e
ogorfti <ww.shanks1@gmail.com>
2023-06-23 07:02:17 -0700

    Add FizzBuzz program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,18 +7,14 @@
  */
 int main(void)
 {
-	int	i;
+	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else if (i != 100)
-			printf("%d ", i);
+		(i % 3 == 0) ? printf("Fizz") : 0;
+		(i % 5 == 0) ? printf("Buzz") : 0;
+		(i % 3 != 0 && i % 5 != 0) ? printf("%d", i) : 0;
+		(i != 100) ? printf(" ") : 0;
 	}
 	printf("\n");
 	return (0);
