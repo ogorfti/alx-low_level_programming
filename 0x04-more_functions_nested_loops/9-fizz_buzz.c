0b43dc62facf4d77878108debd085dfea8e918f3
ogorfti <ww.shanks1@gmail.com>
2023-06-23 06:59:39 -0700

    Add FizzBuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 1 to 100 with FizzBuzz rules.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int	i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
