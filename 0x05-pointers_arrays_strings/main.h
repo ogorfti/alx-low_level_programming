0bad045fd179ee8440247773800cbe74ac9b6d07
ogorfti <ww.shanks1@gmail.com>
2023-06-26 06:48:03 -0700

    add main.h file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef _POINTERS_MAIN_H
+#define _POINTERS_MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+
+void _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+
+#endif
