aa2e1395435d0d9109f746fa14f09ae8929861f2
ogorfti <ww.shanks1@gmail.com>
2023-07-27 06:52:55 -0700

    update header 1.1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: the given string
@@ -23,5 +23,5 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-
+list_t *add_node(list_t **head, const char *str);
 #endif
