7ae715102a615e727bd0eab28e0e411a6337d07e
ogorfti <ww.shanks1@gmail.com>
2023-07-27 07:34:41 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+#define STARTUP_MESSAGE "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+
+/**
+ *before_main - print a message before the main function is executed
+ */
+void  before_main(void)
+{
+	printf(STARTUP_MESSAGE);
+}
+
