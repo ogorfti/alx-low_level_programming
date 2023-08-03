5331d8a7f54d3e01622fe4e061cdbe4a57e79e87
ogorfti <ww.shanks1@gmail.com>
2023-08-03 13:24:00 -0700

    fix case 7

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		num <<= 1;
-		num += b[i] - '0';
+		num += (b[i] - '0');
 		i++;
 	}
 	return (num);
