4c39fa8ba9224cf696688a9bbf858801ed58fee6
ogorfti <ww.shanks1@gmail.com>
2023-08-03 13:49:22 -0700

    Success is not final, failure is not fatal: It is the courage to continue that counts.

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness.
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *ptr = (char *)&num;
+
+	return (*ptr);
+}
+
