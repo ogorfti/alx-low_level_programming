bfe4196ff33b876a12256b9f1efe7f7e80fef44c
ogorfti <ww.shanks1@gmail.com>
2023-08-03 13:18:51 -0700

    fix case 7

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,9 +10,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	int i = 0;
 	
-	if (!b)
-		return (0);
-	while (b[i])
+	while (b && b[i])
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
